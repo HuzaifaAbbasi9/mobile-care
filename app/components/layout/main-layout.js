8c3522b1a0bca366d250a4c71c6f3eb8f102c03f
Huzaifa Iftikhar Abbasi <huzaifaiftikhar671@gmail.com>
2024-07-16 19:50:07 +0500

    test

--- app/components/layout/main-layout.js
+++ app/components/layout/main-layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MainLayout = ({children}) => {
   return (
     <>
-    <header>Header</header>
+    <header>Header 1 test</header>
     <main>{children}</main>
     <footer>Footer</footer>
     </>
